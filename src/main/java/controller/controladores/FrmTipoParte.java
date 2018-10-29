c498781add1e73a3348642391836a86c0174173e
Rafael Garcia <regestra64@gmail.com>
2018-10-29 17:14:17 -0600

    Configuracion de proyecto y entidades creadas

---
+++ src/main/java/controller/controladores/FrmTipoParte.java
@@ -0,0 +1,16 @@
+
+package controller.controladores;
+
+import java.io.Serializable;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author rafael
+ */
+@Named(value = "frmTipoParte")
+@ViewScoped
+public class FrmTipoParte implements Serializable{
+    
+}
---
+++ src/main/java/controller/controladores/FrmTipoVehiculo.java
@@ -0,0 +1,18 @@
+
+package controller.controladores;
+
+import java.io.Serializable;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author rafael
+ */
+@Named(value = "frmTipoVehiculo")
+@ViewScoped
+public class FrmTipoVehiculo implements Serializable{
+
+
+    
+}
