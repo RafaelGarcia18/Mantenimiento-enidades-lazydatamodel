b0bbc96e5dd837dc7ccf5220d3fd01b92ab37dcd
Rafael Garcia <regestra64@gmail.com>
2018-11-03 08:48:16 -0600

    Bug de carga arreglado, vistas funcionando

--- src/main/java/controller/controladores/FrmMarca.java
+++ src/main/java/controller/controladores/FrmMarca.java
@@ -1,13 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller.controladores;
 
+import controller.acceso.MarcaFacade;
 import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
+import org.primefaces.model.LazyDataModel;
+import sv.edu.uesocc.ingenieria.prn335_2018.flota.datos.definicion.Marca;
 
 /**
  *
@@ -15,12 +16,48 @@ import javax.faces.view.ViewScoped;
  */
 @Named(value = "frmMarca")
 @ViewScoped
-public class FrmMarca implements Serializable{
+public class FrmMarca implements Serializable {
 
-    /**
-     * Creates a new instance of FrmMarca
-     */
-    public FrmMarca() {
+    @EJB
+    private MarcaFacade service;
+    private LazyDataModel<Marca> lazyModel;
+    private Marca selectMarca;
+
+    @PostConstruct
+    public void init() {
+        final List<Marca> dataSource = service.findAll();
+        this.lazyModel = new AbstractLazyModel(dataSource) {
+            @Override
+            public Object getRowData(String rowKey) {
+                for (Marca car : dataSource) {
+                    if (car.getIdMarca().toString().equals(rowKey)) {
+                        return car;
+                    }
+                }
+                return null;
+            }
+
+            public Object getRowKey(Marca car) {
+                return car.getIdMarca();
+            }
+
+        };
     }
-    
+
+    public void onRowSelected() {
+        System.out.println("Row selected");
+    }
+
+    public LazyDataModel<Marca> getLazyModel() {
+        return lazyModel;
+    }
+
+    public Marca getSelectMarca() {
+        return selectMarca;
+    }
+
+    public void setSelectMarca(Marca selectMarca) {
+        this.selectMarca = selectMarca;
+    }
+
 }
