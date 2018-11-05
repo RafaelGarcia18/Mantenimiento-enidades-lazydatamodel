1fa03d8ec8be2402ed2feb337c153c9ce4ea5e53
Rafael Garcia <regestra64@gmail.com>
2018-11-05 15:47:16 -0600

    Vista de entidad marca creada, lista para crear composite component de botones

--- src/main/java/controller/controladores/FrmMarca.java
+++ src/main/java/controller/controladores/FrmMarca.java
@@ -2,12 +2,20 @@ package controller.controladores;
 
 import controller.acceso.MarcaFacade;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.event.UnselectEvent;
 import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
 import sv.edu.uesocc.ingenieria.prn335_2018.flota.datos.definicion.Marca;
 
 /**
@@ -22,30 +30,95 @@ public class FrmMarca implements Serializable {
     private MarcaFacade service;
     private LazyDataModel<Marca> lazyModel;
     private Marca selectMarca;
+    private EstadoCRUD estado;
+    private List<Marca> dataSource;
 
     @PostConstruct
     public void init() {
-        final List<Marca> dataSource = service.findAll();
-        this.lazyModel = new AbstractLazyModel(dataSource) {
+        this.estado = EstadoCRUD.NUEVO;
+        this.lazyModel = new LazyDataModel<Marca>() {
             @Override
-            public Object getRowData(String rowKey) {
-                for (Marca car : dataSource) {
-                    if (car.getIdMarca().toString().equals(rowKey)) {
-                        return car;
+            public List<Marca> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+                dataSource = new ArrayList<>();
+                try {
+                    this.setRowCount(service.count());
+                    dataSource = service.findRange(first, pageSize);
+                } catch (Exception e) {
+                    Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+                }
+                return dataSource;
+            }
+
+            @Override
+            public int getRowIndex() {
+                return super.getRowIndex(); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public Object getRowKey(Marca object) {
+                if (object != null) {
+                    return object.getIdMarca();
+                }
+                return null;
+            }
+
+            @Override
+            public Marca getRowData(String rowKey) {
+                if (rowKey != null && !rowKey.isEmpty() && this.getWrappedData() != null) {
+                    try {
+                        Integer search = new Integer(rowKey);
+                        for (Marca tu : (List<Marca>) getWrappedData()) {
+                            if (tu.getIdMarca().compareTo(search) == 0) {
+                                return tu;
+                            }
+                        }
+                    } catch (NumberFormatException e) {
+                        Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
                     }
                 }
                 return null;
             }
 
-            public Object getRowKey(Marca car) {
-                return car.getIdMarca();
-            }
-
         };
+        this.selectMarca = new Marca();
+        this.lazyModel.setRowIndex(-1);
     }
 
-    public void onRowSelected() {
-        System.out.println("Row selected");
+    public void btnAgregarHandler(ActionEvent ae) {
+        if (selectMarca != null) {
+            service.create(selectMarca);
+            init();
+        }
+    }
+
+    public void btnCancelarHandler(ActionEvent ae) {
+        init();
+        this.estado = EstadoCRUD.NUEVO;
+    }
+
+    public void btnEditarHandler(ActionEvent ae) {
+        if (selectMarca != null) {
+            service.edit(selectMarca);
+            init();
+        }
+    }
+
+    public void btnEliminarHandler(ActionEvent ae) {
+        if (selectMarca != null) {
+            service.remove(selectMarca);
+            init();
+        }
+    }
+    
+    public void onRowSelect(SelectEvent event) {
+        selectMarca = (Marca) event.getObject();
+        this.estado = EstadoCRUD.EDITAR;
+    }
+
+    public void onRowDeselect(UnselectEvent event) {
+        this.estado = EstadoCRUD.NUEVO;
+        this.selectMarca = new Marca();
+        this.lazyModel.setRowIndex(-1);
     }
 
     public LazyDataModel<Marca> getLazyModel() {
@@ -53,14 +126,19 @@ public class FrmMarca implements Serializable {
     }
 
     public Marca getSelectMarca() {
-        if(selectMarca == null){
-            selectMarca = new Marca();
-        }
         return selectMarca;
     }
 
     public void setSelectMarca(Marca selectMarca) {
         this.selectMarca = selectMarca;
+    }
+
+    public EstadoCRUD getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoCRUD estado) {
+        this.estado = estado;
     }
 
 }
